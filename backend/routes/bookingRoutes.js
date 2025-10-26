8ba8ab02a227161eb26d9c14f02172e705dbaf77
GoelAnshika99 <99goelanshika@gmail.com>
2025-10-25 18:41:37 -0700

    Clear remote repository (remove existing files)

--- backend/routes/bookingRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const { bookProperty, getBookingHistory } = require('../controllers/bookingController');
-const authMiddleware = require('../middleware/authMiddleware');
-const router = express.Router();
-router.post('/:propertyId', authMiddleware, bookProperty);
-router.get('/history', authMiddleware, getBookingHistory);
-module.exports = router;
--- backend/routes/profileRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { updateProfile, getProfile } = require('../controllers/profileController');
-const authMiddleware = require('../middleware/authMiddleware');
-const upload = require('../middleware/uploadMiddleware');
-const router = express.Router();
-router.get('/view', getProfile);
-router.put('/update', upload.single('profilePicture'), updateProfile);
-module.exports = router;
